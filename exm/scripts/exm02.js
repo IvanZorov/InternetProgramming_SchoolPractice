3fa5a33afcb1740d9e4672f9f53f91767a5ce53a
IvanZorov <i_zoro007@abv.bg>
2015-01-11 23:57:39 +0200

    exm02.js

--- exm/scripts/exm02.js
+++ exm/scripts/exm02.js
@@ -4,17 +4,17 @@ $(document).ready(function() {
 	$("#1337").click(function(){
 		var in1 = $("#3").val();
 		var in2 = $("#666").val();
-		var web0 = "http://echo.jsontest.com/" + in1 + "/" + in2;
-		/*
-			var web = "http://echo.jsontest.com/";
-			var res = encodeURIComponent(in1);
-			var res2 = encodeURIComponent(in2);
-			var web3 = web + res + "/" + res2;
-		*/
+		//var web0 = "http://echo.jsontest.com/" + in1 + "/" + in2;
+
+		var web = "http://echo.jsontest.com/";
+		var res = encodeURIComponent(in1);
+		var res2 = encodeURIComponent(in2);
+		var web3 = web + res + "/" + res2;
+
 		if ( in1 == "" || in2 == "") {
 			alert("ERROR: Please enter key and value");
 		} else {
-			$.get(web0, function(result){
+			$.get(web3, function(result){
 				$("div").text("JSON:" + JSON.stringify(result) );
 			});
 		}
